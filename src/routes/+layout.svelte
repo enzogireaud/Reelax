4f2319e4d3e0da2e20a26952e5003374af279ed3
furasshujipushii <enzogireauds@gmail.com>
2023-07-31 16:57:30 +0200

    adding favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import '../style/style.scss';
 </script>
 
+<svelte:head>
+	<title>Reelax - Immersive Xperience</title>
+	<link rel="shortcut-icon" href="/sleeping.png" />
+	<link rel="icon" href="/sleeping.png" />
+</svelte:head>
 <audio autoplay loop={true}>
 	<source src="/music.mp3" type="audio/mpeg" />
 </audio>
